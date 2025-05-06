982db8f34329ac6bf86aaed6294e649344ab4175
RavLamsal <sauravlamsal9815030371@gmail.com>
2025-05-06 18:17:21 +0545

    Update Problem2.py

--- Problem2.py
+++ Problem2.py
@@ -1,6 +1,4 @@
-# import pyttsx3
-# Engline=pyttsx3.init()
-# Engline.say("i want to eat potato")
-# Engline.runAndWait()
-import pyjokes # type: ignore
-print(pyjokes.get_jokes())
+ import pyttsx3
+ Engline=pyttsx3.init()
+ Engline.say("i want to eat potato")
+ Engline.runAndWait()
