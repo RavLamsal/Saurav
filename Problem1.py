220c1c988d6715b8da3dc00ac1fcc43ab99b7751
sauravlamsal <sauravlamsal9815030371@gmail.com>
2025-05-06 18:08:57 +0545

    So this is my first day
    This is my first day, i did some codes in python.. some of it contains like printing simple words.sentence// importing modules// etc etc.

---
+++ Problem1.py
@@ -0,0 +1,13 @@
+#Write a program to print "Twinkle Twinkle Little Star" poem
+print('''Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle Twinkle Little Star
+How I wonder what you are!
+Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle Twinkle Little Stars
+How I wonder what you are!''')
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Saurav
+# Hello i am saurav, or Rav...
+# This is the begining of my new journey, tho i should had started way before
+# Date:5/6/2025 ::: 23/01/2082
