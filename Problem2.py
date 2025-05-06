958078f20c162d45ad2acdaea793cab8f5cff40f
sauravlamsal <sauravlamsal9815030371@gmail.com>
2025-05-06 18:21:43 +0545

    yeah, commit it

--- Problem2.py
+++ Problem2.py
@@ -1,4 +1,4 @@
- import pyttsx3
- Engline=pyttsx3.init()
- Engline.say("i want to eat potato")
- Engline.runAndWait()
+import pyttsx3
+Engline=pyttsx3.init()
+Engline.say("i want to eat potato")
+Engline.runAndWait()
