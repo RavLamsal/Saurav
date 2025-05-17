769283c89ecdb9af4b4e3757ae521191d03d1fc8
sauravlamsal <sauravlamsal9815030371@gmail.com>
2025-05-17 15:01:47 +0545

    updated

--- Problem5.py
+++ Problem5.py
@@ -6,19 +6,39 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-# Ask user what to search
+# Step 1: Ask what the user wants to know
 query = input("What do you want to know about? ")
+speak("You asked about " + query)
 
 try:
-    # Get summary from Wikipedia
+    # Step 2: Try to get a summary directly
     result = wikipedia.summary(query, sentences=2)
     print("Wikipedia says:", result)
     speak(result)
+
 except wikipedia.exceptions.DisambiguationError as e:
-    speak("Your query was too broad. Please be more specific.")
-    print("Too many options:", e.options[:5])
+    # Step 3: If too many meanings, ask user to choose
+    speak("Your word is confusing. Here are some options.")
+    print("Too many options found for your query. Please pick one:")
+
+    options = e.options[:5]  # show only first 5 to keep it simple
+    for i, option in enumerate(options):
+        print(f"{i + 1}. {option}")
+
+    try:
+        choice = int(input("Enter the number of the option you meant (1-5): "))
+        if 1 <= choice <= len(options):
+            new_query = options[choice - 1]
+            result = wikipedia.summary(new_query, sentences=2)
+            print("Wikipedia says:", result)
+            speak(result)
+        else:
+            speak("Invalid choice. Please try again.")
+    except:
+        speak("Oops. I couldn't understand your number.")
+
 except wikipedia.exceptions.PageError:
     speak("Sorry, I couldn't find anything on that topic.")
 except Exception as e:
-    print("Error:", e)
+    print("Unexpected error:", e)
     speak("Something went wrong.")
